195b3534c6f54aea0ffd3bb508d1081f1badfc9e
Walkerjz <walkerjz@dukes.jmu.edu>
2024-12-07 09:46:45 -0500

    Q2 works

--- Q2 impact of pressure.py
+++ Q2 impact of pressure.py
@@ -29,7 +29,7 @@ psipath = ['Data/1800um/Tare1810um10psi.jpg','Data/1800um/Tare1810um15psi.jpg','
 #canny thresholds of 0 and 207 work for the psipath files to answer question 3 and a filter d=5
 
 
-#edges = []
+
 for file in psipath:
     image = cv2.imread(file)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -37,39 +37,26 @@ for file in psipath:
     cv2.namedWindow('Edge')
     CanEdges = cv2.Canny(filt, 0, 207, apertureSize=5)
     X, Y = extract_edges(CanEdges)
-    #edges.append([X,Y])
-    z = np.polyfit(X,Y,3,)
+    z = np.polyfit(X,Y,3)
     p = np.poly1d(z)
     dom = linspace(300,450, 450-300)
 
-
-
-    plt.subplot(1,3,1)
+    plt.subplot(1,2,2)
     ax1 = plt.gca()
     #ax.invert_yaxis()
     plt.plot(dom,p(dom),'-',label = file)
 
 
-    plt.subplot(1, 3, 2)
+    plt.subplot(1, 2, 1)
     ax2 = plt.gca()
     #ax.invert_yaxis()
     plt.scatter(X,Y,label = file)
-
-
-    plt.subplot(1, 3, 3)
-    ax3 = plt.gca()
-    #ax.invert_yaxis()
-    plt.imshow(cv2.add(gray,CanEdges), cmap='gray')
 
 #ax = plt.gca()
 # invert y-axis to match image coordinates
 ax1.invert_yaxis()
 ax2.invert_yaxis()
-ax3.invert_xaxis()
 
 # show plot
 plt.legend()
 plt.show()
-
-
-pass
